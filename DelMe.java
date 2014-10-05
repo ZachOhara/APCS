7b52a050890923ff31d773dd232220209efcdd35
Zach Ohara <zjohara@gmail.com>
2014-10-04 19:46:08 -0700

    First Sync

---
+++ DelMe.java
@@ -0,0 +1,18 @@
+//import java.util.Scanner;
+
+public class DelMe {
+   public static void main(String[] args) {
+//      Scanner input = new Scanner(System.in);
+//      input.next() or input.nextInt()
+      oddStuff();
+   }
+   
+   public static void oddStuff() {
+      for(int i = 0; i<4; i++) {
+         for (int j = 0; j<=3; j++){
+            System.out.print("+");
+         }
+      }
+   }
+   
+}
---
+++ OharaSongOrig.java
@@ -0,0 +1,30 @@
+public class OharaSongOrig
+{   
+   static String[] objects = { "maiden all forlorn,",
+                           "cow with the crumpled horn,",
+                           "dog,",
+                           "cat,",
+                           "rat,",
+                           "malt,",
+                           "house that Jack built." };
+                           
+   static String[] verbs = { "", "milked", "tossed", "worried", "killed", "ate", "lay in" };
+   
+   static int totalVerses = 7;
+   
+   static void verse(int verseNum) {
+      System.out.println("This is the " + objects[totalVerses - verseNum]);
+      for (int item = verseNum-1; item > 0; item--){
+         System.out.println("That " + verbs[totalVerses - item] + " the " + objects[totalVerses - item]);
+      }
+   }
+   
+   public static void main(String[] args) {
+      for (int verse = 1; verse <= totalVerses; verse++){
+         verse(verse);
+         System.out.println();
+      } 
+      
+   }
+   
+}
