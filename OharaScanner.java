9ad67a0553e6762e8f0d9290f5499e71740c6b9e
Zach Ohara <zjohara@gmail.com>
2014-10-08 12:25:59 -0700

    Scanner Classes

---
+++ OharaScanner.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+
+public class OharaScanner {
+
+	public static void main(String[] args) {
+		inputBirthday();
+		processName(new Scanner(System.in));
+	}
+	
+	public static void inputBirthday() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("On what day of the month were you born? ");
+		String day = input.next();
+		System.out.print("What is the name of the month in which you were born? ");
+		String month = input.next();
+		System.out.print("During what year were you born? ");
+		String year = input.next();
+		System.out.println("You were born on " + month + " " + day 
+				+ ", "+ year + ". You're mighty old!");
+		//input.close(); //uncommenting this line causes an exception; not sure why
+	}
+	
+	public static void processName(Scanner s) {
+		System.out.print("Please enter your full name: ");
+		String firstName = s.next();
+		String revName = s.next() + ", " + firstName;
+		
+		System.out.println("Your name in reverse order is " + revName);
+	}
+
+}
