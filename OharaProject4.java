95534b259c0f04f9aecd67d60d49b650d9610dc9
Zach Ohara <ZachOhara@users.noreply.github.com>
2014-10-31 14:10:09 -0700

    Removed enum
    need to debug

--- OharaProject4.java
+++ OharaProject4.java
@@ -2,6 +2,10 @@ import java.util.*;
 
 public class OharaProject4 {
 	
+	public static final String ROCK = "r";
+	public static final String PAPER = "p";
+	public static final String SCISSORS = "s";
+	
 	public static Scanner console = new Scanner(System.in);
 
 	public static void main(String[] args) {
@@ -32,32 +36,31 @@ public class OharaProject4 {
 	public static void printStats() {
 		// code to print final statistics
 	}
-
-}
-
-enum Weapon {
 	
-	ROCK, PAPER, SCISSORS;
-	
-	static int winner(Weapon a, Weapon b) {
-		if (a == ROCK) {
-			if (b == SCISSORS)
+	// returns 1 or 2 if a string 'wins', 0 if they're the same, and -1 for error
+	static int winner(String a, String b) {
+		a = a.toLowerCase();
+		b = b.toLowerCase();
+		// easier than using equalsIgnoreCase() on every comparison
+		if (a.equals(ROCK)) {
+			if (b.equals(SCISSORS))
 				return 1;
 			else
 				return 2;
-		} else if (a == SCISSORS) {
-			if (b == PAPER)
+		} else if (a.equals(SCISSORS)) {
+			if (b.equals(PAPER))
 				return 1;
 			else
 				return 2;
-		} else if (a == PAPER) {
-			if (b == ROCK)
+		} else if (a.equals(PAPER)) {
+			if (b.equals(ROCK))
 				return 1;
 			else
 				return 2;
-		} else if (a == b) {
+		} else if (a.equals(b)) {
 			return 0;
 		} else //unexpected
 			return -1;
 	}
+
 }
