87085288da8cb84f6e4acf12ab7d705f8ed78d4d
Zach Ohara <zjohara@gmail.com>
2014-11-13 00:43:08 -0800

    More array assignments

---
+++ OharaMoreArrays.java
@@ -0,0 +1,84 @@
+import java.util.Arrays;
+
+/* Problem answers:
+ * 
+ * 1. On line 12 (the 'x++' in mystery) there is no semicolon.
+ *    2 [0, 1]
+ *    1 [0, 1]
+ *    1 [1, 2]
+ *    0 [1, 2]
+ * 
+ */
+public class OharaMoreArrays {
+
+	public static void main(String[] args) {
+		String[] s1 = {"belt", "hat", "jelly", "bubble gum"};
+		System.out.println("Avg len of " + Arrays.toString(s1) + " is " + problem1(s1));
+		System.out.println();
+		
+		String[] s2 = {"alpha", "beta", "gamma", "delta", "gamma", "beta", "alpha"};
+		System.out.println("The array " + Arrays.toString(s2) + " is a palindrome? " + problem2(s2));
+		System.out.println();
+		
+		double[] list1 = {16.1,	12.3,	22.2,	14.4};
+		double[] list2 = 	{1.5,	4.3,	7.0,	19.5,	25.1,	46.2}	;
+		System.out.println("The array " + Arrays.toString(list1) + " is sorted? " + isSorted(list1));
+		System.out.println("The array " + Arrays.toString(list2) + " is sorted? " + isSorted(list2));
+		System.out.println();
+		
+		int[] bar = {3,	8,	12,	2,	9,	17,	43, -8,	46};
+		int[] baz = {4,	7,	3,	9,	12,	-47,	3,	74};
+		System.out.println("The array " + Arrays.toString(bar) + " is unique? " + isUnique(bar));
+		System.out.println("The array " + Arrays.toString(baz) + " is unique? " + isUnique(baz));
+}
+	
+	public static double problem1(String[] list) {
+		double lengthSum = 0;
+		for (String s: list)
+			lengthSum += s.length();
+		return lengthSum / list.length;
+	}
+	
+	public static boolean problem2(String[] list) {
+		boolean isPalindrome = true;
+		for (int i = 0; i < list.length/2; i++)
+			if (!list[i].equals(list[list.length - 1 - i]))
+				isPalindrome = false;
+		return isPalindrome;
+	}
+	
+	// problem 3
+	public static boolean isSorted(double[] foo) {
+		boolean isSorted = true;
+		for (int i = 1; i < foo.length; i++)
+			if (foo[i] < foo[i-1])
+				isSorted = false;
+		return isSorted;
+	}
+	
+	// problem 4
+	public static boolean isUnique(int[] foo) {
+		boolean isUnique = true;
+		for (int i = 0; i < foo.length; i++)
+			for (int j = i + 1; j < foo.length; j++)
+				if (foo[i] == foo[j])
+					isUnique = false;
+		return isUnique;
+	}
+	
+	// problem 5
+	// this method returns the amount of odd numbers in an array plus the number of evens
+	// this is different from the length of the array only in that this is much slower and more pointless
+	public static int mystery(int[] a) { //sound familiar?
+		int odds = 0;
+		int evens = 0;
+		for (int i : a) {
+			if (i%2 == 0)
+				evens++;
+			else
+				odds++;
+		}
+		return evens + odds;
+	}
+
+}
--- given/ArrayDisputes.java
+++ given/ArrayDisputes.java
@@ -3,42 +3,34 @@ package given;
 import java.util.Arrays;
 
 public class ArrayDisputes {
-	
+
 	public static void main(String[] args) {
-		int[] list = {8, 10, 8, 6, 4, 2};
-		System.out.println(mystery1(list));
+		mystery();
 	}
-	
-	public static void objectPassingDispute() {
-		int[] a1 = {1, 3, 5, 7, 9};
-		int[] a2 = {1, 4, 9, 16, 25};
-		System.out.println(Arrays.toString(a1));
-		System.out.println(Arrays.toString(a2));
-		mystery(a1, a2);
-		System.out.println(Arrays.toString(a1));
-		System.out.println(Arrays.toString(a2));
+
+	public static void mystery() {
+		int	x =	1;
+		int[] a	= new int[2];
+		mystery1(x, a);
+		System.out.println(x +	" "	+ Arrays.toString(a));
+		x--;
+		a[1]	=	a.length;
+		mystery1(x,	a);
+		System.out.println(x +	" "	+ Arrays.toString(a));
 	}
-	
-	public static void mystery(int[] a, int[] b) {
-		for (int i = 0; i < a.length; i++)
-			a[i] += b[b.length - 1 - i];
-	}
-	
+
 	public static void forLoopDispute() {
 		int[] numbers = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
 		for (int i = 1; i < 10; i++)
-		     numbers[i] = numbers[i - 1];
+			numbers[i] = numbers[i - 1];
 		System.out.println(Arrays.toString(numbers));
 	}
-	
-	public static int mystery1(int[] list) {
-		int x = 0;
-		for (int i = 0; i < list.length - 1; i++) {
-			if (list[i] > list[i + 1]) {
-				x++;
-			}
-		}
-		return x;
+
+
+	public static void mystery1(int	x, int[] list) {
+		list[x]++;
+		x++;
+		System.out.println(x +	" "	+ Arrays.toString(list));
 	}
 
 }
