033bec131fd61fd871b02b93d9b93ddefa8f2937
Zach Ohara <zjohara@gmail.com>
2014-12-10 01:19:33 -0800

    Bank account assignment
    For static variables/methods

---
+++ src/oharaBankAccounts/BankAccount.java
@@ -0,0 +1,23 @@
+package oharaBankAccounts;
+
+public class BankAccount {
+
+	private static int objectCount = 0;
+	
+	private String name;
+	private int id;
+	
+	public BankAccount(String myName) {
+		this.name = myName;
+		this.id = objectCount;
+		objectCount++;
+	}
+	
+	public static int getNumAccounts() {
+		return objectCount;
+	}
+	
+	public int getID() {
+		return id;
+	}
+}
