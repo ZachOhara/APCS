13d81d11d3ea009d7c5ec7ddbf14b3e9e640d717
Zach Ohara <zjohara@gmail.com>
2015-01-21 00:13:08 -0800

    Arraylist ABCD assignment

---
+++ src/OharaABCD.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+
+public class OharaABCD {
+
+	public static void main(String[] args) {
+		ArrayList<String> letterList = new ArrayList<String>();
+		letterList.add("a");	
+		letterList.add("b");	
+		letterList.add("c");	
+		letterList.add("d");	
+		System.out.println(letterList);	
+		
+		// Add the 'plus' with incrementing index
+		
+		for (int i = 1; i <= letterList.size() - 1; i += 2) {
+			letterList.add(i, " plus ");
+		}
+		System.out.println(letterList);	
+		
+		// Remove the 'plus' with incrementing index 
+		
+		for (int i = 2; i < letterList.size(); i += 1) {
+			letterList.remove(i);
+		}
+		System.out.println(letterList);	
+		
+		// Add the 'plus' with decrementing index 
+		
+		for (int i = letterList.size() - 1; i > 0; i--) {
+			letterList.add(i, " plus ");
+		}
+		System.out.println(letterList);	
+		
+		// Remove the 'plus' with decrementing index
+		
+		for (int i = letterList.size() - 2; i > 0; i -= 2) {
+			letterList.remove(i);
+		}
+		System.out.println(letterList);	
+
+	}
+
+}
