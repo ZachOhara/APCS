a19f74ec12db8247127981e42df628591dab1ebd
Zach Ohara <zjohara@gmail.com>
2015-02-17 02:05:55 -0800

    Finished TextFormatter

--- src/oharaChatbot/frq/TextFormatter.java
+++ src/oharaChatbot/frq/TextFormatter.java
@@ -8,25 +8,27 @@ public class TextFormatter {
     }
     
     public int findString(String str, int start) {
+    	if (start >= line.length())
+    		return -1;
         int pos = start;
         int next;
-        while (pos != -1 && pos < line.length()) {
+        do {
             pos = line.indexOf(str, pos);
             next = line.indexOf(str, pos + 1);
-            if (pos >= 0 && next > pos + 1)
+            if (pos != -1 && next != pos + 1)
                 return pos;
-            pos = pos == -1 ? pos : pos + 3;
-        }
-        return -1;
+            pos = pos == -1 ? pos : pos + 2;
+        } while (pos != -1 && pos < line.length());
+        return pos;
     }
     
     public int countStrings(String str) {
-        int pos = 0;
+        int pos =  -1;
         int found = 0;
-        while (pos != -1) {
-            pos = line.indexOf(str, pos);
+        do {
+            pos = this.findString(str, pos + 1);
             found++;
-        }
+        } while (pos != -1);
         found--;
         return found;
     }
@@ -34,17 +36,16 @@ public class TextFormatter {
     public String convertItalics() {
         int pos1 = -1;
         int pos2 = -1;
-        String result = line;
         do {
-            pos1 = result.indexOf("_", pos2 + 1);
-            pos2 = result.indexOf("_", pos1 + 1);
+            pos1 = findString("_", pos2 + 1);
+            pos2 = findString("_", pos1 + 1);
             if (pos1 != -1 && pos2 != -1) {
-                result = result.substring(0, pos1) + "<I>" + result.substring(pos1 + 2);
+                line = line.substring(0, pos1) + "<I>" + line.substring(pos1 + 1);
                 pos2 += 2;
-                result = result.substring(0, pos2) + "<//I>" + result.substring(pos2 + 2);
+                line = line.substring(0, pos2) + "</I>" + line.substring(pos2 + 1);
                 pos2 += 4;
             }
         } while (pos1 != -1 && pos2 != -1);
-        return result;
+        return line;
     }
 }
