a95e52a895ddcca0dc9e8a442d9da27c1aa1eab4
Zach Ohara <zjohara@gmail.com>
2015-03-02 22:56:37 -0800

    Recursion Assignment

---
+++ src/OharaRecursion.java
@@ -0,0 +1,39 @@
+
+public class OharaRecursion {
+	
+	public static int multiplyEvens(int n) {
+		if (n <= 0)
+			throw new IllegalArgumentException("Cannot accept: " + n);
+		if (n == 1)
+			return 2;
+		else
+			return 2 * n * multiplyEvens(n - 1);
+	}
+	
+	public static void writeSquares(int n) {
+		if (n < 1)
+			throw new IllegalArgumentException("Cannot accept: " + n);
+		if (n == 1)
+			System.out.print("1");
+		else {
+			if (n % 2 == 1)
+				System.out.print((n*n) + ", ");
+			writeSquares(n - 1);
+			if (n % 2 == 0)
+				System.out.print(", " + (n*n));
+		}
+	}
+	
+	public static void main(String[] args) {
+		int[] testCases1 = {1, 2, 3, 4};
+		for (int i : testCases1)
+			System.out.println(multiplyEvens(i));
+		
+		int[] testCases2 = {5, 1, 8};
+		for (int i : testCases2) {
+			writeSquares(i);
+			System.out.println();
+		}
+	}
+
+}
